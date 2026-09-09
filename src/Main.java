390ce6c72f45ef8a7b36acd1d7e23f5b3a91bb6d
IdaCham257 <idacham043@gmail.com>
2026-09-09 15:36:34 +0000

    feat: resized arrays - add items to the arrays using user keyboard

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +18,36 @@ public class Main {
         String[] itemName = {"bread", "berries", "pringles", "apple"};
         double[] itemPrices = {12, 200, 350, 30};
         boolean[] itemAvailable = {true, false, true, false};
+
+        // add items to the Array using Scanner class/object
+
+        do {
+            System.out.println("Are you adding item?: Y or N"); // hint the user
+            String userInput = in.next();
+
+            if (userInput.equalsIgnoreCase("n")) break;
+            System.out.print("Enter item name: ");
+            String name = in.next();
+
+            System.out.print("Enter item price: ");
+            double price = in.nextDouble();
+
+
+            System.out.println("Enter item status: ");
+            boolean status = in.nextBoolean(); // true / false
+
+            System.out.println("Name \t price \t status");
+            System.out.println(name+ "\t" +price+ "\t" +status);
+
+
+            // add new captured item to the store
+            itemName = Arrays.copyOf(itemName,itemName.length+1);
+            // Array is resized {"bread", "berries", "pringles", "apple", ""}
+            itemName[itemName.length-1] = name;
+            System.out.println(Arrays.toString(itemName));
+
+
+        } while (true);
 
         //print the items one-by-one
         for (int i =0; i <itemName.length; i++) {
